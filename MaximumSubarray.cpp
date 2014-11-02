69e77a97806f577e3261fdd20cf1654abf41eaf2
Xin-Tong <tongxinhit@gmail.com>
2014-11-02 11:42:44 -0500

    add the divide and conquer method. O(nlogn)

--- MaximumSubarray.cpp
+++ MaximumSubarray.cpp
@@ -22,3 +22,46 @@ public:
     }
 };
 
+//try coding another solution using the divide and conquer approach, which is more subtle.
+/*
+T(n) = O(1) n=1;
+T(n) = 2T(n/2) + O(n) n>1;
+
+So T(n) = nlogn
+*/
+class Solution {
+public:
+    int Max(int A[], int left, int right, int maxA){
+            if(left>right) return maxA;
+            if(left == right) return A[left];
+            int mid = (left+right)/2;
+            int maxl = Max(A, left, mid, maxA);
+            int maxr = Max(A, mid+1, right, maxA);
+            maxA = max(maxA, maxl);
+            maxA = max(maxA, maxr);
+            int sum = 0; 
+            int maxml = 0;
+            for(int i = mid-1; i>= left; i--){
+                sum += A[i];
+                if(sum > maxml){
+                    maxml = sum;
+                }
+            }
+            sum = 0;
+            int maxmr = 0;
+            for(int i = mid+1; i<= right; i++){
+                sum += A[i];
+                if(sum > maxmr){
+                    maxmr = sum;
+                }
+            }
+            maxA = max(maxA, maxml+maxmr+A[mid]);
+            return maxA;
+        }
+        
+    int maxSubArray(int A[], int n) {
+        int maxA = INT_MIN;
+        maxA = Max(A, 0, n-1, maxA);
+        return maxA;
+    }
+};
